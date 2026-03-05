84ef0c80605240e9e7f410ef21d691469c1557ce
Safiya Hossain <130481780+lvxq37-2@users.noreply.github.com>
2026-03-05 14:13:25 +0000

    feat(nextNodeState): helper function that calculates the next state of the given node using its truth table. Also converted hexadecimal truthtables back to binary.

--- SimpleSim.py
+++ SimpleSim.py
@@ -25,21 +25,35 @@ def inputNodes():
                 print(f"The truthtable does not match expected length for {len(node_neighbourhood)} neighbours.")
 
         # Convert truthtable to hex
-        node_ttable_hex = format(int(node_ttable, 2), "x")
+        # node_ttable_hex = format(int(node_ttable, 2), "x")
 
         ttables[node_letter] = {
             "neighbours": node_neighbourhood,
-            "truthtable": node_ttable_hex
+            "truthtable": node_ttable
         }
 
     return ttables
 
-def nextNodeState(node, current_g_state, ttables):
-    # node is the letter we are calculating the next state for
-    # state represents whether the node is on or off
-
+def nextNodeState(node, current_g_state, ttables, node_order):
     # This function should calculate, based on the node's truthtable of neighbours, what the next state of the node should be
-    pass
+    
+    # Numbers the alphabetically sorted letters and maps them to the current global state in a dictionary
+    state_map = {}
+    for i, letter in enumerate(node_order):
+        state_map[letter] = current_g_state[i]
+
+    # Compile the neighbours' states
+    neighbours = ttables[node]["neighbours"]
+    neighbour_bits = "".join(state_map[nb] for nb in neighbours)
+    index = int(neighbour_bits, 2)
+    
+    # Find this node's state using the truth table
+    new_state = ttables[node]["truthtable"][index]
+
+    # test print
+    print(f"{node} is now {new_state}")
+
+    return new_state
 
 def nextGlobalState(ttables):
     # This function should calculate the next global state, based on the current global state.
@@ -53,7 +67,7 @@ def nextGlobalState(ttables):
     # Find next state of each node
     next_g_state = []
     for node in node_order:
-        next_g_state.append(nextNodeState(node, current_g_state, ttables))
+        next_g_state.append(nextNodeState(node, current_g_state, ttables, node_order))
 
     # Compile and return next global state
     print(f"{"".join(node_order)}: ({current_g_state}) -> ({next_g_state})")
