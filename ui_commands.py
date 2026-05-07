eec34f881c2a17d3709670bbff0c94db01b3d13f
Safiya Hossain <130481780+lvxq37-2@users.noreply.github.com>
2026-05-07 23:57:05 +0100

    refactor(ui_commands.py and ui_main.py): separated UI code into two files, for commands and layout respectively.

---
+++ ui_commands.py
@@ -0,0 +1,291 @@
+import tkinter as tk
+from tkinter import filedialog
+import os
+from PIL import Image, ImageTk
+from io import BytesIO
+
+from boolean_network_simulator import (
+    loadNetworkFromFile, 
+    drawWiringDiagram, 
+    drawStateGraph, 
+    getGraphImageBytes,
+    compileStateTransitions, 
+    runAllTraces, 
+    compileAttractors, 
+    saveTracesToFile,
+    saveAttractorsToFile
+)
+
+# Global variables
+filepath = None
+G = None
+state_trans = None
+
+min_depth = 1
+max_depth = 1000000
+default_depth = 10000
+
+# Widgets from ui_main that need to be accessed in commands
+file_entry=None
+status_label=None
+load_btn=None
+action_frame=None
+wiring_diagram_img=None
+wiring_diagram_title=None
+
+cyclic_var=None
+canonical_var=None
+depth_var=None
+
+def setup_ui(
+        given_file_entry,
+        given_status_label,
+        given_load_btn,
+        given_action_frame,
+        given_wiring_diagram_img,
+        given_wiring_diagram_title,
+        given_cyclic_var,
+        given_canonical_var,
+        given_depth_var,
+):
+    global file_entry, status_label, load_btn, action_frame
+    global wiring_diagram_img, wiring_diagram_title
+    global cyclic_var, canonical_var, depth_var
+
+    file_entry = given_file_entry
+    status_label = given_status_label
+    load_btn = given_load_btn
+    action_frame = given_action_frame
+    wiring_diagram_img = given_wiring_diagram_img
+    wiring_diagram_title = given_wiring_diagram_title
+    cyclic_var = given_cyclic_var
+    canonical_var = given_canonical_var
+    depth_var = given_depth_var
+
+# ------
+# Open file
+# ------
+
+def select_file():
+    """
+    Open a file dialog to select a Boolean network file, and update the UI accordingly.
+    """
+    global filepath
+
+    filepath = filedialog.askopenfilename(
+        title="Open File",
+        filetypes=(("Text Files", "*.txt"), ("All Files", "*.*"))
+    )
+    
+    # If a file was selected, update the entry field and store the path
+    if filepath:
+        file_name = os.path.basename(filepath)
+        
+        file_entry.delete(0, tk.END)
+        file_entry.insert(0, file_name) 
+        
+        # Resets UI to show load button and hide further features
+        resetUI()
+        setStatus(f"File selected: {filepath}")
+
+# ------
+# Button functions
+# ------
+
+def load_network():
+    """
+    Load the Boolean network from the selected file, compile state transitions, and update the UI with the wiring diagram.
+    """
+    global G, state_trans
+
+    # If no file is selected, do nothing
+    if not filepath:
+        return
+    
+    '''
+    TODO: Add error handling for invalid file formats, and display error messages in the status box.
+    '''
+    
+    # Load the network and compile state transitions
+    G = loadNetworkFromFile(filepath)
+    state_trans = compileStateTransitions(G)
+
+    # Create the wiring diagram
+    image_bytes = getGraphImageBytes(G)
+    wiring_diagram = resize_image_bytes(image_bytes)
+
+    revealUI(wiring_diagram)
+    setStatus(f"File loaded: {filepath}")
+    
+
+def draw_wiring_diagram():
+    """
+    Draw the wiring diagram of the Boolean network, then update the status message.
+    """
+    if G is not None:
+        message = drawWiringDiagram(graph=G, filename=filepath)
+        setStatus(message)
+
+def draw_state_diagram():
+    """
+    Draw the state diagram of the Boolean network, then update the status message.
+    """
+    if state_trans is not None:
+        message = drawStateGraph(graph=state_trans, filename=filepath)
+        setStatus(message)
+
+def print_traces():
+    """
+    Run all traces of the Boolean network and save them to a file, then update the status message.
+    """
+    if state_trans is not None:
+        traces = runAllTraces(
+            state_trans,
+            cyclicOnly = cyclic_var.get(),
+            canonicalOrder = canonical_var.get(),
+            maxDepth = get_depth()
+            )
+        
+        message = saveTracesToFile(traces, filepath)
+        setStatus(message)
+
+def print_attractors():
+    """
+    Compile attractors from the Boolean network and save them to a file, then update the status message.
+    """
+    if state_trans is not None:
+        attractors = compileAttractors(
+            state_trans,
+            cyclicOnly = cyclic_var.get(),
+            canonicalOrder = canonical_var.get(),
+            maxDepth = get_depth()
+            )
+        
+        message = saveAttractorsToFile(attractors, filepath)
+        setStatus(message)
+
+
+# ------
+# Depth validation functions
+# ------
+
+def validate_depth(value):
+    """
+    Validate the input for the depth spinbox to ensure it's a positive integer within the specified range. 
+    Values are set to:
+    - minimum of 1
+    - maximum of 1,000,000
+    - default of 10,000
+
+    Input:
+    - value: the input string from the spinbox
+
+    Output:
+    - True if the input is valid (empty string or a valid integer within range), False otherwise
+    """
+    # Allow empty string (to allow user to clear the field)
+    if value == "":
+        return True
+    
+    # Only allow digits
+    if not value.isdigit():
+        return False
+
+    # Minimum limit of 1
+    if int(value) < min_depth or int(value) > max_depth:
+        return False
+
+    return True
+
+def get_depth():
+    """
+    Get the current value of the depth variable, ensuring it is within the valid range. If the value is invalid (e.g., non-integer, out of range), return the default depth.
+    
+    Output:
+    - The valid depth value from the spinbox, or the default depth if the input is invalid
+    """
+    try:
+        value = depth_var.get()
+        if value < min_depth or value > max_depth:
+            raise ValueError("Depth must be between 1 and 1,000,000.")
+
+        return value
+    except (tk.TclError, ValueError):
+        return default_depth
+
+
+# ------
+# Status update function
+# ------
+
+def setStatus(message):
+    """
+    Update the status label with the provided message.
+    
+    Input:
+    - message: the string message to display in the status label
+    """
+    status_label.config(text=message)
+
+
+# ------
+# Image resizer function
+# ------
+
+def resize_image_bytes(image_bytes, max_width=500, max_height=600):
+    """
+    Resize an image from bytes while preserving aspect ratio. Return a PhotoImage for Tkinter.
+    
+    Input:
+    - image_bytes: the image data in bytes format
+    - max_width: the maximum width for the resized image (default is 500 pixels)
+    - max_height: the maximum height for the resized image (default is 600 pixels)
+    
+    Output:
+    - A PhotoImage object that can be used in Tkinter to display the resized image
+    """
+    image = Image.open(BytesIO(image_bytes))
+
+    # Resize the image while preserving aspect ratio
+    image.thumbnail((max_width, max_height))
+
+    return ImageTk.PhotoImage(image)
+
+
+# ------
+# UI update functions
+# ------
+
+def resetUI():
+    """
+    Reset the UI to the initial state after a new file is selected.
+
+    Hides action buttons and clears the wiring diagram.
+    """
+    # Hide action buttons, show load button
+    action_frame.pack_forget()
+    load_btn.pack(pady=10)
+
+    # Hide and clear wiring diagram
+    wiring_diagram_title.pack_forget()
+    wiring_diagram_img.config(image="") 
+    wiring_diagram_img.pack_forget()
+
+def revealUI(wiring_diagram):
+    """
+    Reveal the action buttons and display the wiring diagram after a file is loaded.
+
+    Hides the load button.
+    
+    Input:
+    - wiring_diagram: a PhotoImage object representing the wiring diagram to be displayed
+    """
+    # Show action buttons, hide load button
+    load_btn.pack_forget()
+    action_frame.pack(pady=20)
+
+    # Display the wiring diagram
+    wiring_diagram_title.pack(anchor="nw", pady=(20, 10))
+    wiring_diagram_img.config(image=wiring_diagram)
+    wiring_diagram_img.pack()
+    wiring_diagram_img.image = wiring_diagram  # Keep a reference to prevent garbage collection
---
+++ ui_main.py
@@ -0,0 +1,207 @@
+import tkinter as tk
+import ui_commands as cmds      # Imports commands (and global variables) from ui_commands.py
+
+# ------
+# UI Window
+# ------
+
+root = tk.Tk()
+root.title("Boolean Network Simulator")
+root.geometry("1300x850")
+
+# ----- Layout frames -----
+main_frame = tk.Frame(root)
+main_frame.pack(fill=tk.BOTH, expand=True, padx=20, pady=20)
+
+left_frame = tk.Frame(main_frame)
+left_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+separator = tk.Frame(main_frame, width=2, bg="grey")
+separator.pack(side=tk.LEFT, fill=tk.Y, padx=10)
+
+right_frame = tk.Frame(main_frame, width=500)
+right_frame.pack(side=tk.RIGHT, fill=tk.BOTH)
+right_frame.pack_propagate(False)  # Prevent frame from resizing to fit content
+
+# ----- Settings variables -----
+cyclic_var = tk.BooleanVar(value=False)
+canonical_var = tk.BooleanVar(value=False)
+depth_var = tk.IntVar(value=10000)
+valid_depth_command = root.register(cmds.validate_depth)
+
+# ----- (LEFT) File Selection area -----
+top_frame = tk.Frame(left_frame)
+top_frame.pack(pady=20)
+
+file_entry = tk.Entry(top_frame, width=32)
+file_entry.insert(0, "Select file")
+file_entry.pack(side=tk.LEFT, padx=5)
+
+open_btn = tk.Button(
+    top_frame, 
+    text="Open", 
+    command=cmds.select_file)
+open_btn.pack(side=tk.LEFT)
+
+# Start Button
+load_btn = tk.Button(
+    left_frame, 
+    text="Load Network", 
+    command=cmds.load_network)
+load_btn.pack(pady=10)
+
+# ----- (LEFT) Actions Frame -----
+action_frame = tk.Frame(left_frame)
+
+# -- (LEFT) Visual Selection --
+visual_frame = tk.Frame(action_frame)
+visual_frame.pack(pady=10)
+
+visual_label = tk.Label(
+    visual_frame,
+    text="Visualize Network",
+    width=25,
+    font=("Arial", 11, "bold")
+)
+visual_label.pack(pady=(0, 10))
+
+# Buttons for visualizations
+
+visual_btn_frame = tk.Frame(visual_frame)
+visual_btn_frame.pack()
+
+wiring_btn = tk.Button(
+    visual_btn_frame, 
+    text="Wiring Diagram", 
+    font=("Arial", 10),
+    width=20,
+    command=cmds.draw_wiring_diagram
+)
+wiring_btn.pack(side=tk.LEFT, padx=10)
+
+state_btn = tk.Button(
+    visual_btn_frame, 
+    text="State Transition Diagram", 
+    font=("Arial", 10),
+    width=20,
+    command=cmds.draw_state_diagram
+)
+state_btn.pack(side=tk.LEFT, padx=10)
+
+# -- (LEFT) Analysis Selection --
+analysis_frame = tk.Frame(action_frame)
+analysis_frame.pack(pady=(30, 10))
+
+analysis_label = tk.Label(
+    analysis_frame,
+    text="Analyse Network Dynamics",
+    width=25,
+    font=("Arial", 11, "bold")
+)
+analysis_label.pack(pady=(0, 10))
+
+# Settings for analysis
+settings_frame = tk.Frame(analysis_frame)
+settings_frame.pack(pady=5)
+
+# Settings: Max Depth
+depth_frame = tk.Frame(settings_frame)
+depth_frame.pack(pady=5)
+
+depth_label = tk.Label(depth_frame, text="Max Trace Depth:")
+depth_label.pack(side=tk.LEFT, padx=(0, 5))
+
+depth_spinbox = tk.Spinbox(
+    depth_frame,
+    from_=cmds.min_depth,
+    to=cmds.max_depth,
+    textvariable=depth_var,
+    width=10,
+    validate="key",
+    validatecommand=(valid_depth_command, "%P")
+)
+depth_spinbox.pack(side=tk.LEFT)
+
+# Settings: Checkbox Settings
+cyclic_checkbox = tk.Checkbutton(
+    settings_frame, 
+    text="Cyclic Attractors Only", 
+    variable=cyclic_var
+)
+cyclic_checkbox.pack(anchor="w")    # left align
+
+canonical_checkbox = tk.Checkbutton(
+    settings_frame, 
+    text="Use canonical ordering", 
+    variable=canonical_var
+)
+canonical_checkbox.pack(anchor="w")   # left align
+
+# Buttons for analysis
+
+analysis_btn_frame = tk.Frame(analysis_frame)
+analysis_btn_frame.pack()
+
+trace_btn = tk.Button(
+    analysis_btn_frame, 
+    text="Export Traces", 
+    font=("Arial", 10),
+    width=20,
+    command=cmds.print_traces)
+trace_btn.pack(side=tk.LEFT, padx=10)
+
+attractor_btn = tk.Button(
+    analysis_btn_frame, 
+    text="Export Attractors", 
+    font=("Arial", 10),
+    width=20,
+    command=cmds.print_attractors)
+attractor_btn.pack(side=tk.LEFT, padx=10)
+
+
+# ----- (RIGHT) Status Frame -----
+status_title = tk.Label(
+    right_frame,
+    text="Status:",
+    font=("Arial", 11, "bold"),
+)
+status_title.pack(anchor="nw", pady=(20, 10))    # top left align
+
+status_label = tk.Label(
+    right_frame,
+    text="No file selected.",
+    anchor="nw",
+    justify=tk.LEFT,
+    wraplength=280
+)
+status_label.pack(anchor="nw", pady=(0, 10), fill=tk.BOTH)    # top left align
+
+# ----- (RIGHT) Diagram Frame -----
+
+wiring_diagram_title = tk.Label(
+    right_frame,
+    text="Loaded Network:",
+    font=("Arial", 11, "bold")
+)
+
+wiring_diagram_img = tk.Label(right_frame)
+
+# ----- Initialize UI Commands -----
+
+cmds.setup_ui(
+    file_entry,
+    status_label,
+    load_btn,
+    action_frame,
+    wiring_diagram_img,
+    wiring_diagram_title,
+    cyclic_var,
+    canonical_var,
+    depth_var
+)
+
+# ------
+# Run
+# ------
+
+root.mainloop()
